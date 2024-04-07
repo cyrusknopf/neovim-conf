52c4655ba2965b1a04b47e9e8cb78e8c17781b42
Cyrus Knopf <cyrus.knopf@student.manchester.ac.uk>
2024-04-07 15:42:46 +0100

    init

---
+++ lua/plugins/colorshcheme.lua
@@ -0,0 +1,20 @@
+-- return {
+--     { "rebelot/kanagawa.nvim" },
+--
+--   {
+--     "LazyVim/LazyVim",
+--     opts = {
+--       colorscheme = "kanagawa",
+--     },
+--   },
+-- }
+return {
+  { "shaunsingh/nord.nvim" },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "nord",
+    },
+  },
+}
