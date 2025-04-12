f1521e3bb2c3049ea6b467557d53100c4cd84d28
Cyrus Knopf <cyrus.knopf@gmail.com>
2025-04-13 00:41:15 +0100

    I like

--- init.lua
+++ init.lua
@@ -1,7 +1,61 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+-- Load the Gleam language server, separate to Mason
+require("lspconfig").gleam.setup({})
 
 vim.o.tabstop = 4 -- A TAB character looks like 4 spaces
 vim.o.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
 vim.o.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
 vim.o.shiftwidth = 4 -- Number of spaces inserted when indenting
+
+-- Set text wrapping
+vim.o.wrap = true
+vim.o.linebreak = true
+vim.o.breakindent = true
+vim.o.showbreak = "↪ "
+
+-- macOS PDF viewer settings
+vim.g.vimtex_view_method = "sioyek"
+
+-- Configure vimtex to use pdflatex
+vim.g.vimtex_compiler_method = "pdflatex"
+
+-- Ensure vimtex is loaded
+vim.g.vimtex_enabled = 1
+
+-- Disable pink lines? no
+vim.g.vimtex_quickfix_mode = 0
+
+-- Custom function to clean up auxiliary files
+function CleanAuxFiles()
+  -- List of auxiliary files to remove
+  local aux_files = {
+    "*.aux",
+    "*.log",
+    "*.out",
+    "*.toc",
+    "*.fls",
+    "*.fdb_latexmk",
+    "*.synctex.gz",
+  }
+
+  -- Command to delete the files
+  for _, file_pattern in ipairs(aux_files) do
+    local cmd = "rm " .. file_pattern
+    print("Executing: " .. cmd) -- Debug message
+    os.execute(cmd)
+  end
+end
+
+-- Vimtex compiler hooks
+vim.g.vimtex_compiler_hooks = {
+  build_init = function()
+    CleanAuxFiles()
+  end,
+  build_success = function()
+    CleanAuxFiles()
+  end,
+}
+
+-- Additional vimtex settings (optional)
+vim.g.vimtex_compiler_progname = "neovim-remote" -- Use neovim-remote if using Neovim
--- lua/plugins/colorshcheme.lua
+++ lua/plugins/colorshcheme.lua
@@ -1,20 +1,14 @@
--- return {
---     { "rebelot/kanagawa.nvim" },
---
---   {
---     "LazyVim/LazyVim",
---     opts = {
---       colorscheme = "kanagawa",
---     },
---   },
--- }
 return {
+  { "rebelot/kanagawa.nvim" },
   { "shaunsingh/nord.nvim" },
-
+  { "EdenEast/nightfox.nvim" },
+  { "savq/melange-nvim" },
+  { "neanias/everforest-nvim" },
+  { "bluz71/vim-moonfly-colors", name = "moonfly", lazy = false, priority = 1000 },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "nord",
+      colorscheme = "moonfly",
     },
   },
 }
